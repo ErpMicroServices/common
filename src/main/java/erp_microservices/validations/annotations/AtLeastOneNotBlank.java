4ab87909fe2267e83004b0460656e308627cca32
Jim Barrows <JIm.Barrows@gmail.com>
2018-04-19 13:12:17 -0500

    Added validations.

---
+++ src/main/java/erp_microservices/validations/annotations/AtLeastOneNotBlank.java
@@ -0,0 +1,31 @@
+package erp_microservices.validations.annotations;
+
+import erp_microservices.validations.validators.AtLeastOneNotBlankValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import java.util.logging.Logger;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({TYPE, ANNOTATION_TYPE})
+@Retention(RUNTIME)
+@Constraint(validatedBy = AtLeastOneNotBlankValidator.class)
+@Documented
+public @interface AtLeastOneNotBlank {
+	Logger logger = Logger.getLogger(AtLeastOneNotBlank.class.getName());
+
+	String message() default "{jdo.validations.AtLeastOneNotNull}";
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
+
+	String[] fieldNames();
+
+}
